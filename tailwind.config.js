17cd6bc949da5f4366119e90c64a6b6ca8ae2923
EmCousin <EmCousin@users.noreply.github.com>
2024-03-03 16:49:13 +0000

    deploy: 980b55b6364e9bd2d404efc1afed940a00f2c3d8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,34 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   darkMode: 'media',
   content: [
     './_drafts/**/*.html',
     './_includes/**/*.html',
     './_layouts/**/*.html',
-    './_posts/*.md',
     './*.md',
     './*.html'
   ],
-  corePlugins: {
-    preflight: false
-  },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+           // Class name
+          'animation-duration': (value) => {
+            return {
+              "--animation-duration": value
+            }
+          },
+        },
+        { values: theme('transitionDuration') }
+      )
+    }),
+  ],
   theme: {
     extend: {
       animation: {
-        'slide-in-from-left': 'slide-in-from-left 0.5s both',
-        'slide-in-from-right': 'slide-in-from-right 0.5s both'
+        'slide-in-from-left': 'slide-in-from-left var(--animation-duration) both',
+        'slide-in-from-right': 'slide-in-from-right var(--animation-duration) both'
       },
       keyframes: {
         'slide-in-from-left': {
